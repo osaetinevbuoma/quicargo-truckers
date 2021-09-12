8fe453affb881936f495cc35f234aacd1862ad16
Osaetin Evbuoma <osaetinevbuoma@gmail.com>
2021-09-12 07:04:25 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Quicargo Truckers
---
+++ server/src/env.ts
@@ -0,0 +1,2 @@
+export const SERVER_HOST: string = process.env.SERVER_HOST || 'localhost';
+export const SERVER_PORT: number = Number(process.env.SERVER_PORT) || 3000;
